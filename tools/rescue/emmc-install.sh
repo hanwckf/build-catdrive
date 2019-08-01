547b3c00c5831bdcc8369392644ab55bbd0a9f1e
hanwckf <my375229675@gmail.com>
2019-08-01 22:37:43 +0800

    emmc-install.sh: blink leds

--- tools/rescue/emmc-install.sh
+++ tools/rescue/emmc-install.sh
@@ -7,7 +7,9 @@ disk="/dev/mmcblk0"
 [ ! -e $disk ] && echo "emmc not found!" && exit 1
 
 echo "flash emmc..."
+echo 2 > /sys/class/leds/red/blink
 
 pv -pterb $img | dd of=$disk conv=fsync bs=2M
 
 [ "$?" = "0" ] && echo "flash done, please unplug USB drive and reboot now!" || echo "flash fail!"
+echo 0 > /sys/class/leds/red/blink
