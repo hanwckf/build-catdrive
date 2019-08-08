2935b1af69a9e6f3497c4adc72175a16683034cd
hanwckf <my375229675@gmail.com>
2019-08-09 00:08:31 +0800

    emmc-install.sh: reword

--- tools/rescue/emmc-install.sh
+++ tools/rescue/emmc-install.sh
@@ -11,5 +11,6 @@ echo 2 > /sys/class/leds/red/blink
 
 pv -pterb $img | dd of=$disk conv=fsync bs=2M
 
-[ "$?" = "0" ] && echo "flash done, please unplug USB drive and reboot now!" || echo "flash fail!"
+[ "$?" = "0" ] && echo "flash done, please poweroff now then unplug USB drive!" || echo "flash fail!"
 echo 0 > /sys/class/leds/red/blink
+
